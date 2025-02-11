db8c7a978731440bbd4beaf058db506920143b63
Deepank-01 <deepanksingh01@gmail.com>
2025-02-12 01:23:30 +0530

    V1

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-require("dotenv").config();
+
 const express = require("express");
 var cookieParser = require('cookie-parser')
-
+require("dotenv").config();
 const sequelize = require("./config/dbconnect");
 const User = require("./modal/User");
 const Train=require("./modal/Trains")
